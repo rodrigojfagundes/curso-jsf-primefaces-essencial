dc808071327b3c78b773707587cb914ffd6ec5ae
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-02-13 19:24:51 -0300

    Mapeando as Entidades

---
+++ curso-jsf-e-primefaces-essencial/src/main/java/com/algaworks/erp/model/TipoEmpresa.java
@@ -0,0 +1,23 @@
+package com.algaworks.erp.model;
+
+
+public enum TipoEmpresa {
+	
+	
+	MEI("Microempreendedor Individual"),
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anonima");
+	
+	private String descricao;
+	
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+}
